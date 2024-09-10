e3bb32f181acd9ba4c31e7bf20593ab6c3f81273
random2907 <yarnjeetyadav1234567890@gmail.com>
2024-09-10 19:21:59 +0530

    add button

--- lib/main.dart
+++ lib/main.dart
@@ -4,57 +4,57 @@ import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'package:latlong2/latlong.dart';
 
-
-void main(){
+void main() {
   runApp(const Myapp());
 }
 
-class Myapp extends StatelessWidget{
+class Myapp extends StatelessWidget {
   const Myapp({super.key});
   @override
-    Widget build(BuildContext context){
-      return MaterialApp(
-          title: "Hello world",
-          home: const Homepage(),
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(
-              seedColor: Colors.blue,
-              ),
-            useMaterial3: true,
-            ).copyWith(
-              appBarTheme: const AppBarTheme(
-                backgroundColor: Colors.blue,
-                ),
-              ),
-          darkTheme: ThemeData.from(
-            colorScheme: ColorScheme.fromSeed(
-              seedColor: Colors.blueGrey,
-              ),
-            ).copyWith(
-              appBarTheme: const AppBarTheme(
-                backgroundColor: Colors.blueGrey,
-                ),
-              ),
-            themeMode: ThemeMode.system,
-            );
-    }
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Hello world",
+      home: const Homepage(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+        ),
+        useMaterial3: true,
+      ).copyWith(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blue,
+        ),
+      ),
+      darkTheme: ThemeData.from(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blueGrey,
+        ),
+      ).copyWith(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blueGrey,
+        ),
+      ),
+      themeMode: ThemeMode.system,
+    );
+  }
 }
 
-class Homepage extends StatefulWidget{
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
   @override
-    HomepageState createState() => HomepageState();
+  HomepageState createState() => HomepageState();
 }
 
-class HomepageState extends State<Homepage>{
+class HomepageState extends State<Homepage> {
   Position? position;
   String _locationMessage = "";
 
-  @override 
-    void initState() {
-      super.initState();
-      _initializeLocation();
-    }
+  @override
+  void initState() {
+    super.initState();
+    _initializeLocation();
+  }
+
   Future<void> _initializeLocation() async {
     await _checkGps();
     await _getLocationAndSend();
@@ -63,11 +63,12 @@ class HomepageState extends State<Homepage>{
   Future<void> _checkGps() async {
     bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      LocationPermission locationPermission = await Geolocator.requestPermission();
+      LocationPermission locationPermission =
+          await Geolocator.requestPermission();
       if (locationPermission == LocationPermission.denied) {
         setState(() {
-            _locationMessage = "Location services are disabled.";
-            });
+          _locationMessage = "Location services are disabled.";
+        });
         return;
       }
     }
@@ -79,64 +80,89 @@ class HomepageState extends State<Homepage>{
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
         setState(() {
-            _locationMessage = "Location permissions are denied.";
-            });
+          _locationMessage = "Location permissions are denied.";
+        });
         return;
       }
     }
 
     if (permission == LocationPermission.deniedForever) {
       setState(() {
-          _locationMessage = "Location permissions are permanently denied.";
-          });
+        _locationMessage = "Location permissions are permanently denied.";
+      });
       return;
     }
 
     // Get the current position
     position = await Geolocator.getCurrentPosition();
-    setState((){
-        _locationMessage = "Location fetched: ${position!.latitude}, ${position!.longitude}";
-        });
+    setState(() {
+      _locationMessage =
+          "Location fetched: ${position!.latitude}, ${position!.longitude}";
+    });
 
     String url = 'http://192.168.0.108:8080/';
     await http.post(Uri.parse(url), body: {
-        'latitude': position!.latitude.toString(),
-        'longitude': position!.longitude.toString(),
-        });
+      'latitude': position!.latitude.toString(),
+      'longitude': position!.longitude.toString(),
+    });
   }
 
+  var i = "Punch in";
+
   @override
-    Widget build(BuildContext context){
-      return Scaffold(
-          appBar: AppBar(
-            title: const Text("Attendence"),
-            centerTitle: true,
-            ),
-          body:  position == null
-          ? Center(child: Text(_locationMessage.isEmpty ? "Fetching location..." : _locationMessage))
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Attendence"),
+        centerTitle: true,
+      ),
+      body: position == null
+          ? Center(
+              child: Text(_locationMessage.isEmpty
+                  ? "Fetching location..."
+                  : _locationMessage))
           : FlutterMap(
-            options: MapOptions(
-              initialCenter: LatLng(position!.latitude,position!.longitude),
-              initialZoom: 18,
+              options: MapOptions(
+                initialCenter: LatLng(position!.latitude, position!.longitude),
+                initialZoom: 18,
               ),
-            children: [
-            TileLayer(
-              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-              userAgentPackageName: 'dev.fleaflet.flutter_map.example',
-              ),
-            MarkerLayer(
-              markers:[
-              Marker(
-                point: LatLng(position!.latitude,position!.longitude),
-                width: 80,
-                height: 80,
-                child: const Icon( Icons.location_on, color: Colors.red),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+                ),
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      point: LatLng(position!.latitude, position!.longitude),
+                      width: 80,
+                      height: 80,
+                      child: const Icon(Icons.location_on, color: Colors.red),
+                    ),
+                  ],
+                ),
+                Stack(
+                  children: <Widget>[
+                    Align(
+                      alignment: AlignmentDirectional.bottomCenter,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          _initializeLocation();
+                          setState(() {
+                            if (i == "Punch in") {
+                              i = "Punch out";
+                            } else {
+                              i = "Punch in";
+                            }
+                          });
+                        },
+                        child: Text(i),
+                      ),
+                    ),
+                  ],
                 ),
               ],
-              ),
-            ],
             ),
-            );
-    }
+    );
+  }
 }
-
